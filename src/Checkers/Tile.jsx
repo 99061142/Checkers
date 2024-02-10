3caca49030f7e33ab11f15910bacf1f963004c10
99061142 <xarbprive@gmail.com>
2024-02-10 20:34:47 +0100

    Render a tile (with a stone if needed)

---
+++ src/Checkers/Tile.jsx
@@ -0,0 +1,69 @@
+import { Component } from "react";
+import Stone from "./Stone";
+
+class Tile extends Component {
+    constructor() {
+        super();
+        this.state = {
+            stone: null
+        };
+    }
+
+    componentDidMount() {
+        // Return if the tile has a stone when initializing
+        const hasStone = () => {
+            if (
+                this.props.row === 3 ||
+                this.props.row === 4
+            ) {
+                return false
+            }
+
+            if (this.props.row % 2 === 0)
+                return this.props.col % 2 !== 0
+            return this.props.col % 2 === 0
+        }
+
+        // If the tile has no stone when initializing, return
+        if (!hasStone())
+            return
+
+        // Set the stone
+        this.setState({
+            stone: <Stone
+                backgroundColor={(this.props.row < 3) ? "white" : "red"}
+            />
+        });
+    }
+
+    get stone() {
+        const stone = this.state.stone;
+        return stone
+    }
+
+    get backgroundColor() {
+        // Return the background color of the tile
+        if (this.props.row % 2 === 0) {
+            if (this.props.col % 2 === 0)
+                return "white"
+            return "black"
+        }
+        if (this.props.col % 2 === 0)
+            return "black"
+        return "white"
+    }
+
+    render() {
+        return (
+            <td
+                style={{
+                    backgroundColor: this.backgroundColor
+                }}
+            >
+                {this.stone}
+            </td>
+        )
+    }
+}
+
+export default Tile;
