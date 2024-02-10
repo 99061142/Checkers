0ffc353b3353cea6e50c17ac5e500ad01c7684fb
99061142 <xarbprive@gmail.com>
2024-02-10 20:24:29 +0100

    Render the board

--- src/Checkers/App.js
+++ src/Checkers/App.js
@@ -1,9 +1,10 @@
 import { Component } from "react";
+import Board from "./Board";
 
 class App extends Component {
     render() {
         return (
-            <></>
+            <Board />
         );
     }
 }
