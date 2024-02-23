ab2c1b7bb5beca3cffa785040632ed37cd655801
99061142 <xarbprive@gmail.com>
2024-02-23 20:29:18 +0100

    Changed stones state list to dict with pos as keys

--- src/Checkers/Board.jsx
+++ src/Checkers/Board.jsx
@@ -6,16 +6,41 @@ class Board extends Component {
         super();
         this.state = {
             tilePixelSize: 0,
-            stones: []
+            stones: {},
+            size: [8, 8]
         };
         this._table = createRef(null);
-
-        //! Use the same value [8, 10] for the "rows" and "cols" var
-        this._rows = 8;
-        this._cols = 8;
     }
 
     componentDidMount() {
+        this.props.setBoardComponentMounted(true);
+        this.adjustTable(this.size);
+    }
+
+    get rows() {
+        const rows = this.size[0];
+        return rows
+    }
+
+    get cols() {
+        const cols = this.cols[0];
+        return cols
+    }
+
+    get size() {
+        const size = this.state.size;
+        return size
+    }
+
+    set size(size) {
+        this.setState({
+            size
+        }, () => this.adjustTable(size));
+    }
+
+
+    adjustTable(size) {
+        const [rows, cols] = size
         const adjustTableSize = () => {
             // Assign the lowest size between the width and height as width and height size.
             //! This is needed to create a board with only the given rows and columns
@@ -31,37 +56,41 @@ class Board extends Component {
             );
 
             // Set the size for each tile
-            this.tilePixelSize = lowestSize / (this._rows / 2) / 2;
+            this.tilePixelSize = lowestSize / (rows / 2) / 2;
         }
         adjustTableSize();
 
-
-        const midRow = this._rows / 2;
-        const posHasStone = (row, col) => {
-            if (
-                row === midRow ||
-                row === midRow - 1
-            ) {
-                return false
+        const adjustStonesPosition = () => {
+            const midRow = rows / 2;
+            const posHasStone = (pos) => {
+                const [row, col] = pos
+                if (
+                    row === midRow ||
+                    row === midRow - 1
+                ) {
+                    return false
+                }
+                if (row % 2 === 0)
+                    return col % 2 !== 0
+                return col % 2 === 0
             }
-            if (row % 2 === 0)
-                return col % 2 !== 0
-            return col % 2 === 0
-        }
 
-        const stones = [];
-        for (let row = 0; row < this._rows; row++) {
-            const stonesRow = [];
-            for (let col = 0; col < this._cols; col++) {
-                const stoneData = posHasStone(row, col) ? {
-                    ref: createRef(null),
-                    player: row < midRow ? 1 : 2
-                } : null;
-                stonesRow.push(stoneData);
+            const stones = {};
+            for (let row = 0; row < rows; row++) {
+                for (let col = 0; col < cols; col++) {
+                    const pos = [row, col];
+                    if (!posHasStone(pos))
+                        continue
+
+                    stones[pos] = {
+                        ref: createRef(null),
+                        player: row < midRow ? 1 : 2
+                    };
+                }
             }
-            stones.push(stonesRow);
+            this.setStones(stones);
         }
-        this.setStones(stones);
+        adjustStonesPosition();
     }
 
     setStones = (stones) => {
@@ -99,19 +128,16 @@ class Board extends Component {
                 }}
                 ref={this._table}
             >
-                {this.stones
-                    .map((stonesRow, row) =>
-                        stonesRow
-                            .map((stoneData, col) =>
-                                stoneData !== null &&
-                                <Stone
-                                    pos={[row, col]}
-                                    maxSize={this.tilePixelSize}
-                                    player={stoneData.player}
-                                    ref={stoneData.ref}
-                                    key={col}
-                                />
-                            )
+                {Object
+                    .entries(this.stones)
+                    .map(([pos, data], key) =>
+                        <Stone
+                            maxSize={this.tilePixelSize}
+                            pos={pos.split(',').map((direction => Number(direction)))}
+                            player={data.player}
+                            ref={data.ref}
+                            key={key}
+                        />
                     )
                 }
             </div>
