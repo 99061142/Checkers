3fc6c554eac00702050f4cd94f3667a7b81dc86d
99061142 <xarbprive@gmail.com>
2024-03-11 20:16:02 +0100

    Added functionality to get the movable positions
    * Added function "currentPlayerMovablePositions" to get the movable positions based on the current player, and chosen rules (chosen rules gets added at a later date).
    
    The "movablePositions" state (list with every movable position) gets changed after every change of the board size, or current player.

--- src/Checkers/Board.jsx
+++ src/Checkers/Board.jsx
@@ -7,14 +7,13 @@ class Board extends Component {
         this.state = {
             tilePixelSize: 0,
             board: {},
-            size: null // Set when mounted
+            size: null, // Set when mounted
+            movablePositions: [] // Update every board size change
         };
-        this._table = createRef(null);
+        this._ref = createRef(null);
     }
 
     componentDidMount() {
-        //    this.props.setBoardComponentMounted(true); //! NOT YET NEEDED
-
         // Set the size for the board
         this.size = 8;
     }
@@ -59,7 +58,7 @@ class Board extends Component {
         const adjustTableSize = () => {
             // Assign the lowest size between the width and height as width and height size.
             //! This is needed to create a board with only the given rows and columns
-            const tableElement = this._table.current;
+            const tableElement = this._ref.current;
             const tableRect = tableElement.getBoundingClientRect();
             const lowestSize = Math.min(tableRect.width, tableRect.height);
             Object.assign(
@@ -100,7 +99,10 @@ class Board extends Component {
                         continue
 
                     const player = row < midRow ? 1 : 2;
-                    board[pos] = player;
+                    board[pos] = {
+                        ref: createRef(null),
+                        player
+                    };
                 }
             }
             this.setBoard(board);
@@ -111,7 +113,9 @@ class Board extends Component {
     setBoard = (board) => {
         this.setState({
             board
-        });
+        },
+            () => this.movablePositions = this.currentPlayerMovablePositions
+        );
     }
 
     get board() {
@@ -137,7 +141,12 @@ class Board extends Component {
 
         // Return which player is on the current position
         const board = this.board;
-        const player = board[pos];
+        const posData = board[pos];
+
+        if (!posData)
+            return null
+
+        const player = board[pos].player;
         return player
     }
 
@@ -154,9 +163,47 @@ class Board extends Component {
         return true
     }
 
+    get currentPlayerMovablePositions() {
+        let movablePositions = [];
+        for (const [pos, posData] of Object.entries(this.board)) {
+            // If the stone isn't used by the current player, continue
+            if (posData.player !== this.props.currentPlayer)
+                continue
+
+            // If there are no possible moves, continue
+            const possibleMoves = posData.ref.current.possibleMoves;
+
+            if (!Object.keys(possibleMoves).length)
+                continue
+
+            // Add the stone pos to the movable positions list
+            movablePositions.push(pos);
+        }
+        return movablePositions
+    }
+
+    get movablePositions() {
+        const movablePositions = this.state.movablePositions;
+        return movablePositions
+    }
+
+    set movablePositions(movablePositions) {
+        this.setState({
+            movablePositions
+        });
+    }
+
+    componentDidUpdate(prevProps) {
+        // If the player's turn is over, update the movable positions
+        if (prevProps.currentPlayer !== this.props.currentPlayer)
+            this.movablePositions = this.currentPlayerMovablePositions;
+    }
+
+
     render() {
         return (
             <div
+                ref={this._ref}
                 className="position-absolute m-auto top-0 bottom-0 start-0 end-0 border border-dark"
                 style={{
                     width: "75%",
@@ -165,18 +212,19 @@ class Board extends Component {
                     backgroundBlendMode: "difference, normal",
                     backgroundSize: this.tilePixelSize * 2 + "px " + this.tilePixelSize * 2 + "px"
                 }}
-                ref={this._table}
             >
                 {Object
                     .entries(this.board)
-                    .map(([pos, player], key) =>
+                    .map(([pos, { ref, player }]) =>
                         <Stone
+                            ref={ref}
                             posPlayer={this.posPlayer}
                             posInBounds={this.posInBounds}
                             maxSize={this.tilePixelSize}
-                            pos={pos.split(',').map((direction => Number(direction)))}
+                            pos={pos.split(',').map((v => Number(v)))}
                             player={player}
-                            key={key}
+                            movablePositions={this.movablePositions}
+                            key={pos}
                         />
                     )
                 }
