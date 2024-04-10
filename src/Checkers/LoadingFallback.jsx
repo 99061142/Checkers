f2d63043416a23b03ce4f8e1acb454a7d0fb52e2
99061142 <xarbprive@gmail.com>
2024-04-10 18:25:21 +0200

    Added spinner to the loadings page

--- src/Checkers/LoadingFallback.jsx
+++ src/Checkers/LoadingFallback.jsx
@@ -3,9 +3,14 @@ import { Component } from "react";
 class LoadingFallback extends Component {
     render() {
         return (
-            <div>
-                <h1>Loading...</h1>
-            </div>
+            <div
+                className="position-absolute m-auto top-0 bottom-0 start-0 end-0 spinner-border"
+                role="status"
+                style={{
+                    width: "10rem",
+                    height: "10rem"
+                }}
+            />
         )
     }
 }
