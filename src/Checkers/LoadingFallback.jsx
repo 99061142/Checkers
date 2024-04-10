5226be233645494612c18f8e8343b7249eca5fa5
99061142 <xarbprive@gmail.com>
2024-04-10 17:38:26 +0200

    Loading fallback page
    Show this page when the loaded page isn't completed.

---
+++ src/Checkers/LoadingFallback.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class LoadingFallback extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+}
+
+export default LoadingFallback;
