0b9722c2853fc0fad52e477e9928fe9d15a3a1c1
99061142 <xarbprive@gmail.com>
2024-04-16 00:01:15 +0200

    Form for the board settings

---
+++ src/Checkers/settings/BoardSettingsForm.jsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import { FormCheck } from 'react-bootstrap';
+
+class BoardSettingsForm extends Component {
+    boardSizeChanged(ev) {
+        const target = ev.target;
+        const id = target.id;
+        const value = target.checked;
+        this.props.updateFormSettings(id, value);
+    }
+
+    render() {
+        return (
+            <>
+                <fieldset>
+                    <legend>
+                        Board size
+                    </legend>
+                    {this.props.formSettings.boardSize.optionalValues
+                        .map((optionalValue, key) =>
+                            <FormCheck
+                                defaultChecked={this.props.formSettings.boardSize.value === optionalValue}
+                                onChange={(ev) => this.boardSizeChanged(ev)}
+                                type="radio"
+                                id={"boardSize-" + key}
+                                name="boardSize"
+                                value={optionalValue}
+                                label={optionalValue + "X" + optionalValue}
+                                title={null}
+                                key={key}
+                            />
+                        )}
+                </fieldset>
+            </>
+        )
+    }
+}
+
+export default BoardSettingsForm;
