c8535a1aa9e8df120177fc65a8ab88966a93fe92
99061142 <xarbprive@gmail.com>
2024-10-02 11:27:10 +0200

    Changed state naming
    Changed setCurrentComponentName  to setCurrentComponent.
    
    Changed separate currentComponentName & previousComponentName states to component -> current or previous.

--- src/Checkers/Window.jsx
+++ src/Checkers/Window.jsx
@@ -10,66 +10,76 @@ class Window extends Component {
     constructor() {
         super();
         this.state = {
-            currentComponentName: "Game",
-            previousComponentName: null
+            component: {
+                current: "Game",
+                previous: null
+            }
         }
     }
 
-    setCurrentComponentName = (componentName) => {
+    get currentComponent() {
+        // Get the current component that is shown
+        const currentComponent = this.state.component.current;
+        return currentComponent
+    }
+
+    get previousComponent() {
+        // Get the previous component that was shown
+        const previousComponent = this.state.component.previous;
+        return previousComponent
+    }
+
+    setCurrentComponent = (newComponent) => {
+        // Set the previous component as currentComponent and the currentComponent as newComponent (this func parameter)
         this.setState(state => {
             return {
-                previousComponentName: state.currentComponentName,
-                currentComponentName: componentName
+                component: {
+                    previous: state.component.current,
+                    current: newComponent
+                }
             }
         });
     }
 
-    get previousComponentName() {
-        return this.state.previousComponentName;
-    }
-
-    get currentComponentName() {
-        return this.state.currentComponentName
-    }
-
     render() {
+        // Render one of the component based on the currentComponent state, if there is no possible case throw an RangeError
         return (
             <Suspense
                 fallback={<LoadingFallback />}
             >
                 {(() => {
-                    switch (this.currentComponentName) {
+                    switch (this.currentComponent) {
                         case "MainMenu":
                             return <MainMenu
-                                setCurrentComponent={this.setCurrentComponentName}
+                                setCurrentComponent={this.setCurrentComponent}
                             />
                         case "Settings":
                             return <Settings
-                                setCurrentComponent={this.setCurrentComponentName}
-                                previousComponent={this.previousComponentName}
+                                setCurrentComponent={this.setCurrentComponent}
+                                previousComponent={this.previousComponent}
                                 settings={this.settings}
                                 setSettings={this.setSettings}
                             />
                         case "Game":
                             return <Game
-                                setCurrentComponent={this.setCurrentComponentName}
+                                setCurrentComponent={this.setCurrentComponent}
                                 settings={this.settings}
                                 setSettings={this.setSettings}
                                 updateSettings={this.updateSettings}
                             />
                         case "EscapeMenu":
                             return <EscapeMenu
-                                setCurrentComponent={this.setCurrentComponentName}
+                                setCurrentComponent={this.setCurrentComponent}
                                 currentComponent={this.currentComponent}
                                 settings={this.settings}
                                 setSettings={this.setSettings}
                             />
                         case "About":
                             return <About
-                                setCurrentComponent={this.setCurrentComponentName}
+                                setCurrentComponent={this.setCurrentComponent}
                             />
                         default:
-                            throw RangeError("The component named: \"" + this.currentComponentName + "\" couldn't be rendered. Check if the name is correctly spelled, or if it's an optional switch case.")
+                            throw RangeError("The component named: \"" + this.currentComponent + "\" couldn't be rendered. Check if the name is correctly spelled, or if it's an optional switch case.")
                     }
                 })()}
             </Suspense>
