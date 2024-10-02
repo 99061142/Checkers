d1f44e03b777d04a220946c55782e9910e32e94f
99061142 <xarbprive@gmail.com>
2024-10-02 11:27:47 +0200

    Fixed error in updateSettings handling
    Fixed error where some settings couldn't be changed.

--- src/Checkers/App.js
+++ src/Checkers/App.js
@@ -10,24 +10,70 @@ class App extends Window {
     }
 
     get settings() {
-        return this.state.settings
+        // Return the global settings (json file)
+        const settings = this.state.settings;
+        return settings
     }
 
     setSettings = (settings) => {
+        // Set the global settings (json file)
         this.setState({
             settings
         });
     }
 
-    updateSettings = (keys, values) => {
-        const updatedSettings = JSON.parse(JSON.stringify(this.settings));    
-        for(const [i, key] of keys.entries()) {
-            const newValue = values[i];
-            if (updatedSettings[key].constructor === Object)
-                updatedSettings[key].value = newValue;
-            else
-                updatedSettings[key] = newValue;
+updateSettings = (keys, values) => {
+        function newSettingValueIsSameType(key, newValue, currentValue) {
+            // Return if the value of the newValue and currentValue are the same types
+            // If not return a TypeError
+            const newValueType = typeof newValue;
+            const currentValueType = typeof currentValue;
+            if (newValueType === currentValueType)
+                return true
+            throw TypeError(`The new value (${newValue} (${newValueType})) for the ${key} setting must be the same type as the old value (${currentValue} (${currentValueType}))`)
         }
+
+        // Update the global settings based on the keys and values parameter
+        // e.g.
+        // keys = ["gameRunning", "gameRules-canCaptureBackwards"] values = [true, false]
+        // Changes the gameRunning -> value to true, and the gameRules -> canCaptureBackwards -> value to false.
+        const updatedSettings = JSON.parse(JSON.stringify(this.settings));
+        for (const key of keys) {
+            const recurseKeys = key.split('-');
+            const newSettingValue = values.shift();
+            let settingValueChanged = false;
+            let setting = updatedSettings;
+            for (const recurseKey of recurseKeys) {
+                // If the currentSettingValue is an child object, set the setting to currentSettingValue and continue
+                const currentSettingValue = setting[recurseKey]
+                if (typeof currentSettingValue === "object") {
+                    setting = currentSettingValue;
+                    continue
+                }
+
+                // If the value of the currentSettingValue is undefined, return a TypeError
+                if (currentSettingValue === undefined)
+                    throw TypeError(`The setting ${key} couldn't be found`);
+
+                // Set the new setting value if the value of the current setting value and new setting value is the same type,
+                // else throw a TypeError
+                if (newSettingValueIsSameType(key, newSettingValue, currentSettingValue)) {
+                    setting[recurseKey] = newSettingValue;
+                    settingValueChanged = true;
+                    continue
+                }
+            }
+            // If the setting was already changed, continue
+            if(settingValueChanged)
+                continue
+
+            // Set the new setting value if the value of the current setting value and new setting value is the same type,
+            // else throw a TypeError
+            const currentSettingValue = setting.value;
+            if (newSettingValueIsSameType(key, newSettingValue, currentSettingValue))
+                setting.value = newSettingValue;
+        }
+        // Set the changed settings
         this.setSettings(updatedSettings);
     }
 }
