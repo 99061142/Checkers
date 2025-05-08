6adae44fd7901babbe8e9f05ae44fe4c5053f276
99061142 <70692479+99061142@users.noreply.github.com>
2025-05-08 22:51:21 +0200

    Initialize the settings when opening the  app
    Set the defaultSettings in the localstorage if it isn't set yet.
    
    If set, check if any settings are missing, and add them

--- src/Checkers2/App.js
+++ src/Checkers2/App.js
@@ -1,7 +1,16 @@
 import { Component } from 'react';
+import { initializeSettings } from './settings/settingsData'
 import Window from './Window';
+import './app.scss';
 
 class App extends Component {
+    componentDidMount() {
+        // Initialize the settings when the app is opened
+        // This will check if the settings are already set in local storage, and if not, it will set them to the default settings
+        // It will also update the localstorage if any settings are missing, which could happen when an update is made to the defaultSettings.json file, while the user already has the old version of the localstorage settings
+        initializeSettings();
+    }
+    
     render() {
         return (
             <Window />
