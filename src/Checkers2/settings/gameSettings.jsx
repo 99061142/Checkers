2818345d484fd8bd43e5f97cd6f9d03d080ef69b
99061142 <70692479+99061142@users.noreply.github.com>
2025-05-08 23:35:22 +0200

    Added Settings for the game
    Added the setting to change the gamemode (pve or pvp), and the settings to change specific game rules to  enable or disable the game rule.

---
+++ src/Checkers2/settings/gameSettings.jsx
@@ -0,0 +1,75 @@
+import { Component } from 'react';
+import { FormCheck, Form } from 'react-bootstrap';
+import './settingsStyling.scss';
+
+class GameSettings extends Component {
+    gamemodeChanged = (ev) => {
+        // Update the gamemode based on the selected value
+        const settingName = ev.target.name;
+        const settingValue = ev.target.value;
+        this.props.updateSettingValue(settingName, settingValue);
+    }
+
+    gameRuleChanged = (ev) => {
+        // Update the game rule (true/false) based on the selected value
+        // This is used to enable/disable the game rule
+        const settingName = ev.target.name + "-" + ev.target.value;
+        const settingValue = ev.target.checked;
+        this.props.updateSettingValue(settingName, settingValue);
+    }
+    
+    render() { 
+        return (
+            <>
+            <Form>
+                <fieldset>
+                    <legend>
+                        Gamemode
+                    </legend>
+                    {["pve", "pvp"]
+                        .map((optionalGamemode, key) =>
+                            <FormCheck
+                                onChange={this.gamemodeChanged}
+                                defaultChecked={this.props.settings.gamemode === optionalGamemode}
+                                type="radio"
+                                className="text-uppercase"
+                                id={"_gamemode-" + key}
+                                name="gamemode"
+                                value={optionalGamemode}
+                                label={optionalGamemode}
+                                title={null}
+                                key={key}
+                            />
+                        )}
+                </fieldset>
+            </Form>
+            <Form>
+                <fieldset>
+                    <legend>
+                        Game rules
+                    </legend>
+                        {Object
+                            .entries(this.props.settings.gameRules)
+                            .map(([gameRule], key) =>
+                                <FormCheck
+                                    onChange={this.gameRuleChanged}
+                                    defaultChecked={this.props.settings.gameRules[gameRule]}
+                                    type="switch"
+                                    disabled={this.props.settings.gameRunning}
+                                    id={"_gameRule-" + key}
+                                    name="gameRules"
+                                    value={gameRule}
+                                    label={gameRule.charAt(0).toUpperCase() + gameRule.slice(1).replace(/([A-Z])/g, ' $1').toLowerCase()}
+                                    title={null}
+                                    draggable={false}
+                                    key={key}
+                                />
+                            )}
+                </fieldset>
+            </Form>
+        </>
+        );
+    }
+}
+
+export default GameSettings;
