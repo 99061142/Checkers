d7233fe392143f796e1a287fd1ef6364b6515179
99061142 <70692479+99061142@users.noreply.github.com>
2025-05-13 10:30:57 +0200

    Temporary escape menu styling
    Termpory styling for the escape menu, which is the same as for the main menu.

--- src/Checkers/EscapeMenu.jsx
+++ src/Checkers/EscapeMenu.jsx
@@ -34,11 +34,59 @@ class EscapeMenu extends Component {
     }
 
     render() {
+        const buttonStyling = {
+            background: "linear-gradient(45deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000), linear-gradient(135deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000)",
+            border: "3px #000 solid",
+            fontSize: "2vw",
+            color: "black"
+        };
+
         return (
-            <div>
-                <button onClick={() => this.props.toggleComponent("Game")}>Resume Game</button>
-                <button onClick={() => this.props.toggleComponent("Settings")}>Settings</button>
-                <button onClick={() => this.props.toggleComponent("MainMenu")}>Main Menu</button>
+            <div
+                className="d-flex flex-column justify-content-center align-items-center"
+                style={{
+                    height: "100vh",
+                    width: "100vw",
+                    background: "linear-gradient(45deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000), linear-gradient(135deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000)"
+                }}
+            >
+                <h1
+                    className="text-center"
+                    style={{
+                        fontSize: "7.5vw",
+                        marginBottom: "10vh"
+                    }}
+                >
+                    Escape Menu
+                </h1>
+                <div
+                    className="d-flex gap-5 flex-column"
+                    style={{
+                        width: "40vw"
+                    }}
+                >
+                    <button
+                        className="rounded-5 py-4"
+                        onClick={() => this.props.toggleComponent("Game")}
+                        style={buttonStyling}
+                    >
+                        Resume Game
+                    </button>
+                    <button
+                        className="rounded-5 py-4"
+                        onClick={() => this.props.toggleComponent("Settings")}
+                        style={buttonStyling}
+                    >
+                        Settings
+                    </button>
+                    <button
+                        className="rounded-5 py-4"
+                        onClick={() => this.props.toggleComponent("MainMenu")}
+                        style={buttonStyling}
+                    >
+                        Main Menu
+                    </button>
+                </div>
             </div>
         );
     }
