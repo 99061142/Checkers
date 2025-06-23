edd634b6b75bba82bdf1d9a2990476279b470852
99061142 <70692479+99061142@users.noreply.github.com>
2025-06-23 10:30:58 +0200

    Removed switchPlayer prop
    Removed switchPlayer prop from the Stones component. And also made the comments more clear within the Board component.

--- src/Checkers/game/Board.tsx
+++ src/Checkers/game/Board.tsx
@@ -1,30 +1,28 @@
 import { Component } from 'react';
 import { getBoardSize } from '../settings/settingsData.ts';
-import { LastPlayer } from './gameData.ts';
+import { CurrentPlayer, GameData } from './gameData.ts';
 import Stones from './Stones.tsx';
 
 /**
  * Props for the Board component.
  * - gameDataPresent: A boolean value indicating whether the game data is present in the local storage.
- * - markGameOver: Function to mark the game as over.
  * - setWinner: Function to set the winner of the game.
- * - currentPlayer: The current player (1 or 2).
+ * - gameData: The game data object containing the current player and board state.
  * - gameOver: A boolean value indicating whether the game is over or not.
- * - switchPlayer: Function to switch the current player.
+ * - setGameData: Function to set the game data.
  */
 interface BoardProps {
     gameDataPresent: boolean;
-    markGameOver: (isGameOver: boolean) => void;
-    setWinner: (player: LastPlayer) => void;
-    currentPlayer: LastPlayer;
+    setWinner: (player: CurrentPlayer) => void;
+    gameData: GameData;
     gameOver: boolean;
-    switchPlayer: () => void;
+    setGameData: (gameData: GameData) => void;
 }
 
 /**
  * State for the Board component.
- * - boardPixelSize: The size of the board in pixels, this will be the same for both the width and height of the board.
- * - tilePixelSize: The size of the tiles in pixels, this will be the same for both the width and height of the tiles.
+ * - boardPixelSize: The size of the board in pixels. This will be the same for both the width and height of the board.
+ * - tilePixelSize: The size of the tiles in pixels. This will be the same for both the width and height of the tiles.
  */
 interface BoardState {
     boardPixelSize: number;
@@ -32,8 +30,8 @@ interface BoardState {
 }
 
 class Board extends Component<BoardProps, BoardState> {
-    boardHasChanged = false; // Flag to check if the board has changed
-    boardSize = getBoardSize(); // Number of tiles per row and column on the board
+    // The number of tiles per row and column on the board
+    boardSize = getBoardSize();
 
     constructor(props: BoardProps) {
         super(props);
@@ -46,7 +44,7 @@ class Board extends Component<BoardProps, BoardState> {
     componentDidMount() {
         window.addEventListener('resize', this.resizeHandler);
 
-        // Initialize the board and tile sizes when the component mounts
+        // Initialize the board and tile sizes when the component is mounted
         this.updateBoardPixelSizes();
     }
 
@@ -60,24 +58,24 @@ class Board extends Component<BoardProps, BoardState> {
      */
     resizeHandler = (): void => {
         // Update the board and tile sizes when the window is resized
-        this.updateBoardPixelSizes();   
+        this.updateBoardPixelSizes();
     }
 
     /**
-     * Initializes and updates the board and tile sizes based on the current window size.
-     * This will ensure that the board is always a square and that the tiles are always the same size.
+     * Set the board and tile sizes based on the current window size.
+     * This will ensure that the board and tiles are always a square.
      * @returns {void}
      */
     updateBoardPixelSizes(): void {
-        // Percentage of the parent container that the board should occupy
-        const boardSizePercentOfContainer = 75;
+        // Percentage of the parent container which the board should occupy
+        const boardSizePercentageOfContainer = 75;
 
-        // Set the maximum board height and width based on the window size and the percentage of the container
-        const maxBoardHeight = Math.floor(window.innerHeight * (boardSizePercentOfContainer / 100));
-        const maxBoardWidth = Math.floor(window.innerWidth * (boardSizePercentOfContainer / 100));
+        // Set the maximum board height and width based on the window size and the percentage we want the board to occupy
+        const maxBoardHeight = Math.floor(window.innerHeight * (boardSizePercentageOfContainer / 100));
+        const maxBoardWidth = Math.floor(window.innerWidth * (boardSizePercentageOfContainer / 100));
 
         // Choose the smallest size between the height and width of the screen.
-        // This will ensure that the board is always a square, and that the tiles are always the same size
+        // This will ensure that the board is always a square
         const boardPixelSize = Math.floor(Math.min(maxBoardHeight, maxBoardWidth) / this.boardSize) * this.boardSize;
 
         // Calculate the tile size based on the board size divided by the number of tiles per row/column
@@ -104,12 +102,11 @@ class Board extends Component<BoardProps, BoardState> {
                 }}
             >
                 <Stones
+                    setWinner={this.props.setWinner}
                     tileSize={this.state.tilePixelSize}
                     gameDataPresent={this.props.gameDataPresent}
-                    markGameOver={this.props.markGameOver}
-                    setWinner={this.props.setWinner}
-                    currentPlayer={this.props.currentPlayer}
-                    switchPlayer={this.props.switchPlayer}
+                    gameData={this.props.gameData}
+                    setGameData={this.props.setGameData}
                 />
             </div>
         )
