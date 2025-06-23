934fe456a0f1e3ef0eb29a13d21a1fbde50cbcca
99061142 <70692479+99061142@users.noreply.github.com>
2025-06-23 10:25:28 +0200

    Removed some functions, and added clearer comments
    Added comments to make the funcitonality of the component more clear. And removed some uneccessary functions which we would set in another component (e.g. switchPlayer).

--- src/Checkers/game/Game.tsx
+++ src/Checkers/game/Game.tsx
@@ -1,152 +1,164 @@
-import { Component } from 'react';
-import { getLastPlayer, setLastPlayer, clearGameData, LastPlayer } from './gameData.ts';
+import { Component, lazy, Suspense } from 'react';
+import { getCurrentPlayer, clearGameData, CurrentPlayer, setGameData, GameData, getBoard, getInitialBoardGrid } from './gameData.ts';
 import { getInitialPlayer } from '../settings/settingsData.ts';
-import GameOverOverlay from './GameOverOverlay.tsx';
 import Board from './Board.tsx';
-
+import LoadingFallback from '../LoadingFallback.tsx';
+const GameOverOverlay = lazy(() => import('./GameOverOverlay.tsx'));
 
 /**
  * Props for the Game component.
  * - gameDataPresent: A boolean value indicating whether the game data is present in the local storage.
+ * - setGameDataPresent: Function to set the game data present flag.
  * - toggleComponent: Function to toggle the current component.
  */
 interface GameProps {
     gameDataPresent: boolean;
+    setGameDataPresent: (flag: boolean) => void;
     toggleComponent: (componentName: string) => void;
 }
 
 /**
  * State for the Game component.
- * - currentPlayer: The current player (1 or 2).
  * - gameOver: A boolean value indicating whether the game is over or not.
  * - winner: The player that won the game, or null if the game is not finished.
+ * - gameData: An object containing the game data.
  */
 interface GameState {
-    currentPlayer: LastPlayer
     gameOver: boolean;
-    winner: LastPlayer | null;
+    winner: CurrentPlayer | null;
+    gameData: GameData
 }
 
 class Game extends Component<GameProps, GameState> {
-    lastPlayerHasChanged = false; // Flag to check if the last player has been changed
+    // Flag which indicdates if a change has been made to the game data.
+    gameHasChanged = false;
 
     constructor(props: GameProps) {
         super(props);
         this.state = {
-            currentPlayer: this.props.gameDataPresent ? getLastPlayer()! : getInitialPlayer(),
             gameOver: false,
-            winner: null
+            winner: null,
+            gameData: {
+                currentPlayer: this.props.gameDataPresent ? getCurrentPlayer()! : getInitialPlayer(),
+                board: this.props.gameDataPresent ? getBoard()! : getInitialBoardGrid()
+            }
         };
     }
 
-    componentDidMount() {
+    /**
+     * @return {void}
+     */
+    componentDidMount(): void {
         window.addEventListener('keydown', this.keydownHandler);
         window.addEventListener('beforeunload', this.beforeUnloadHandler);
     }
 
-    componentWillUnmount() {
+    /**
+     * Lifecycle method that is called when the component is about to be unmounted.
+     * - If the game is not over, it will save the game data to the local storage.
+     * - If the game is over, it will clear the game data from the local storage.
+     * @return {void}
+     */
+    componentWillUnmount(): void {
         window.removeEventListener('keydown', this.keydownHandler);
         window.removeEventListener('beforeunload', this.beforeUnloadHandler);
 
-        // If the last player has changed, save the current player to the local storage
-        if (this.lastPlayerHasChanged) {
+        // If the game is over, clear the game data, otherwise save it if the game has changed.
+        if (this.state.gameOver) {
+            clearGameData();
+            this.props.setGameDataPresent(false);
+        } else {
             this.saveGameData();
         }
     }
 
     /**
-     * Saves the current component game data to the local storage.
-     * This function is called when the game is closed.
-     * 
-     * Note: Only the data in the component's state is saved. Other game data which is created in other components are not saved in this function.
+     * Saves the game data to the local storage if the game has been changed.
      * @returns {void}
      */
     saveGameData(): void {
-        // If the last player has been changed, save the current player to the local storage
-        if (this.lastPlayerHasChanged) {
-            setLastPlayer(this.state.currentPlayer);
+        // If the game has been changed, save the game data to the local storage
+        if (this.gameHasChanged) {
+            setGameData(this.state.gameData);
         }
     }
 
     /**
-     * Handles the keydown event
+     * Handler for the keydown event.
      * @param {KeyboardEvent} ev - The keyboard event triggered by the user.
      * @returns {void}
      */
     keydownHandler = (ev: KeyboardEvent): void => {
+        // If the Escape key is pressed and the game is not over, toggle the escape menu component
         if (ev.key === "Escape" && !this.state.gameOver) {
-            this.props.toggleComponent("EscapeMenu");
+            this.props.toggleComponent("escapeMenu");
         }
     }
 
     /**
-     * Sets the winner of the game.
-     * @param {LastPlayer} player - The player that won the game.
+     * Sets the winner of the game in the component's state, and set the gameOver state to true.
+     * @param {CurrentPlayer} player - The player that won the game.
      * @return {void}
      */
-    setWinner = (player: LastPlayer): void => {
+    setWinner = (player: CurrentPlayer): void => {
         this.setState({
-            winner: player
+            winner: player,
+            gameOver: true
         });
     }
 
     /**
-     * Mark if the game is over or not.
-     * @param {boolean} isGameOver - A boolean value indicating whether the game is over or not.
-     * @return {void}
+     * Sets the game data in the component's state.
+     * 
+     * This function also marks that the game has been changed, which is used to determine if the game data should be saved when the component is unmounted.
+     * @param {GameData} gameData - The game data object containing the current player and grid of the board.
+     * @returns {void}
      */
-    markGameOver = (isGameOver: boolean): void => {
+    setGameData = (gameData: GameData): void => {
         this.setState({
-            gameOver: isGameOver
+            gameData
+        }, () => {
+            // Mark that the game data has been changed.
+            // This is used to determine if the game data should be saved when the component is unmounted
+            this.gameHasChanged = true;
+
+            // Mark the game data as present.
+            // This is used to determine if some settings could be changed, or if we could load a saved game
+            this.props.setGameDataPresent(true);
         });
     }
 
     /**
-     * Switch the current player.
-     * If the current player is 1, set it to 2, else set it to 1.
-     *
-     * Also marks the last player as changed, so that the player is saved to the local storage when the game is closed.
+     * Handler for the beforeunload event.
      * @return {void}
      */
-    switchPlayer = (): void => {
-        this.setState(prevState => ({
-            currentPlayer: prevState.currentPlayer === 1 ? 2 : 1
-        }));
-
-        // Mark the last player as changed
-        this.lastPlayerHasChanged = true;
-    }
-
-    /**
-     * Save the game data before exiting the game if the game is not over.
-     */
-    beforeUnloadHandler = () => {
-        // If the game is over, clear the game data, and return
-        if (this.state.gameOver) {
-            clearGameData();
-            return
+    beforeUnloadHandler = (): void => {
+        // If the game is not over, save the game data to the local storage
+        if (!this.state.gameOver) {
+            this.saveGameData();
         }
-
-        // Save the game data which is present in the component's state
-        this.saveGameData();
     }
 
     render() {
         return (
             <>
                 {this.state.gameOver && this.state.winner !== null &&
-                    <GameOverOverlay 
-                        toggleComponent={this.props.toggleComponent}
-                        winner={this.state.winner}
-                    />
+                    <Suspense 
+                        fallback={<LoadingFallback />}
+                    >
+                        <GameOverOverlay 
+                            toggleComponent={this.props.toggleComponent}
+                            winner={this.state.winner}
+                            setGameDataPresent={this.props.setGameDataPresent}
+                        />
+                    </Suspense>
                 }
                 <Board
+                    gameData={this.state.gameData}
+                    setGameData={this.setGameData}
                     gameDataPresent={this.props.gameDataPresent}
-                    markGameOver={this.markGameOver}
                     setWinner={this.setWinner}
                     gameOver={this.state.gameOver}
-                    currentPlayer={this.state.currentPlayer}
-                    switchPlayer={this.switchPlayer}
                 />
             </>
         )
