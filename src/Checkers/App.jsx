8cdec5d98991f64ce4895fa6e211fb23019d2c1b
99061142 <70692479+99061142@users.noreply.github.com>
2025-06-23 10:08:03 +0200

    Changed comment to make it more clear

--- src/Checkers/App.jsx
+++ src/Checkers/App.jsx
@@ -5,9 +5,7 @@ import './app.scss';
 
 class App extends Component {
     componentDidMount() {
-        // Initialize the settings when the app is opened
-        // This will check if the settings are already set in local storage, and if not, it will set them to the default settings
-        // It will also update the localstorage if any settings are missing, which could happen when an update is made to the defaultSettings.json file, while the user already has the old version of the localstorage settings
+        // Initialize the settings when the application starts
         initializeSettings();
     }
 
