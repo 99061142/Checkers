4cddcf1c390daa0ecd5f6127b48d8a423cf67d54
99061142 <70692479+99061142@users.noreply.github.com>
2025-08-18 22:51:01 +0200

    Refactor LoadingFallback to functional component
    Converted LoadingFallback from a class component to a functional component.

--- src/Checkers/LoadingFallback.tsx
+++ src/Checkers/LoadingFallback.tsx
@@ -1,18 +1,16 @@
-import { Component } from 'react';
+import { FC } from 'react';
 
-class LoadingFallback extends Component {
-    render() {
-        return (
-            <div
-                className="spinner-border"
-                role="status"
-                style={{
-                    width: "10rem",
-                    height: "10rem"
-                }}
-            />
-        );
-    }
+const LoadingFallback: FC = () => {
+    return (
+        <div
+            className="spinner-border"
+            role="status"
+            style={{
+                width: "10rem",
+                height: "10rem"
+            }}
+        />
+    );
 }
 
 export default LoadingFallback;
