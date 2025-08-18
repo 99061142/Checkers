16921fc4c7b61a6dfd42cc6373848d467374e394
99061142 <70692479+99061142@users.noreply.github.com>
2025-08-18 23:56:30 +0200

    Add settings storage context and provider
    *Created SettingsStorageContext and SettingsStorageProvider for managing the settings across the application.
    
    * We validate the stored/changed settings when:
    1. We validate the stored settings when the application opens.
    2. We validate and store the settings when the setting component gets closed.
    3. We validate and store the settings when the application gets closed.
    
    * If the validation fails when we do so, we set the failed settings to the initial settings. If some of those settings are important for the game mechanics which are saved, we delete the saved data.

---
+++ src/Checkers/settings/settingsStorage/settingsStorage.tsx
@@ -0,0 +1,159 @@
+import { createContext, FC, ReactNode, useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
+import { GameSettingValues, getInitialGameSettingValues, storeSettingValuesWithinLocalStorage, validateSettingValues, getGameSettingOptions, getStoredSettingValues, ValidationErrors, SettingValues } from './settingsStorageUtils.ts';
+import { useGameStorageContext } from '../../game/gameStorage/gameStorage.tsx';
+
+const useSettingsStorage = () => {
+    const {
+        isGameDataPresent,
+        deleteGameData
+    } = useGameStorageContext();
+
+    const [isSettingFormShown, setIsSettingFormShown] = useState<boolean>(false);
+    const [gameSettings, setGameSettings] = useState<GameSettingValues>(getStoredSettingValues()?.gameSettings || getInitialGameSettingValues()); 
+    const gameSettingOptions = getGameSettingOptions();
+
+    /**
+     * Returns an object containing the current setting values.
+     * @returns {SettingValues} - The current setting values.
+     */
+    const currentSettingValues = useMemo((): SettingValues => {
+        const currentSettingValues: SettingValues = {
+            gameSettings
+        };
+        return currentSettingValues;
+    }, [gameSettings]);
+
+    /**
+     * * logs error messages for incorrect values. (values which are of the wrong type, or not one of the allowed values)
+     * * logs info messages for missing properties. (values that are expected but not present)
+     * * logs debug messages for unexpected properties. (values that are present but not expected)
+     * @param {ValidationErrors} errors - The validation errors to log.
+     * @returns {void}
+     */
+    const logErrorMessagesForInvalidSettings = useCallback((errors: ValidationErrors): void => {
+        // Logs the error messages for the incorrect values.
+        // This could be due to the value being of the wrong type, or the value is not one of the allowed values.
+        // We log these as errors, since they are critical issues that need to be addressed.
+        const incorrectValueErrors = errors.incorrectValueErrors;
+        for (const errorMessage of incorrectValueErrors) {
+            console.error(errorMessage);
+        }
+
+        // Logs the error messages for the missing properties.
+        // This could be due to the `initialSettings.json` file being changed from the previous time the application was run.
+        // We log these as `info`, since they are not critical issues, but gives context to the user about the missing properties.
+        const missingPropertyErrors = errors.missingPropertyErrors;
+        for (const errorMessage of missingPropertyErrors) {
+            console.info(errorMessage);
+        }
+
+        // Logs the error messages for the unexpected properties.
+        // This could be due to the `initialSettings.json` file being changed from the previous time the application was run.
+        // We log these as `debug`, since they are not critical issues, but gives context to the developer about the unexpected properties.
+        const unexpectedPropertyErrors = errors.unexpectedPropertyErrors;
+        for (const errorMessage of unexpectedPropertyErrors) {
+            console.debug(errorMessage);
+        }
+    }, []);
+
+    /**
+     * Validates the settings and persists them within the local storage.
+     * * If some setting properties are missing, incorrect, or unexpected, it logs the error messages.
+     * * Afterwards, it stores the validated settings within the local storage.
+     * * If the validation result indicates that the game data must be reset, it deletes the game data.
+     * @param {SettingValues} settingsToValidate - The settings to validate.
+     * @returns {void}
+     */
+    const validateAndPersistSettings = useCallback((settingsToValidate: SettingValues): void => {
+        const validationResult = validateSettingValues(settingsToValidate);
+        const { errors, validatedSettingValues, mustResetGameData } = validationResult;
+
+        logErrorMessagesForInvalidSettings(errors);
+
+        // We always store the `validatedSettingValues` within the local storage, since we want to persist the settings.
+        // We use the `validatedSettingValues` to ensure that the settings are valid before storing them. 
+        // If the settings were not changed by the validation, the `validatedSettingValues` will be the same as the `settingsToValidate`.
+        storeSettingValuesWithinLocalStorage(validatedSettingValues)
+
+        // If the validation result indicates that the game data must be reset, we delete the game data.
+        // This is done to ensure that the game data is in a valid state, since the settings have changed.
+        if (
+            mustResetGameData && 
+            isGameDataPresent
+        ) {
+            deleteGameData();
+        }
+    }, [logErrorMessagesForInvalidSettings, deleteGameData, isGameDataPresent]);
+
+    // When the `SettingsStorageProvider` mounts, it validates the settings and persists them within the local storage.
+    const isInitialValidationValidatedRef = useRef(false);
+    useEffect(() => {
+        if (isInitialValidationValidatedRef.current) {
+            return;
+        }
+        isInitialValidationValidatedRef.current = true;
+
+        validateAndPersistSettings(currentSettingValues);
+    }, [currentSettingValues, validateAndPersistSettings]);
+
+    // When the `isSettingFormShown` state changes from `true` to `false`, which indicates that the settings form has been closed, we validate and persist the settings.
+    const previousIsSettingFormShownState = useRef(isSettingFormShown);
+    useEffect(() => {
+        // If the form is closed after being opened
+        if (
+            previousIsSettingFormShownState.current && 
+            !isSettingFormShown
+        ) {
+            validateAndPersistSettings(currentSettingValues);
+        }
+        
+        previousIsSettingFormShownState.current = isSettingFormShown;
+    }, [isSettingFormShown, currentSettingValues, validateAndPersistSettings]);
+
+    // Add the `beforeunload` event listener to validate and persist the settings when the user tries to close the application, while still being within the settings form.
+    useEffect(() => {
+        /** 
+         * If the user tries to close the application while still being within the settings form, we validate and persist the settings.
+         * @returns {void}
+         */
+        const beforeunloadHandler = (): void => {
+            if (isSettingFormShown) {
+                validateAndPersistSettings(currentSettingValues);
+            }
+        };
+
+        window.addEventListener('beforeunload', beforeunloadHandler);
+        return () => {
+            window.removeEventListener('beforeunload', beforeunloadHandler);
+        }
+    }, [validateAndPersistSettings, currentSettingValues, isSettingFormShown]);
+
+    return {
+        gameSettings,
+        setGameSettings,
+        gameSettingOptions,
+        setIsSettingFormShown
+    };
+}
+
+
+const SettingsStorageContext = createContext<ReturnType<typeof useSettingsStorage> | null>(null);
+
+export const SettingsStorageProvider: FC<{ children: ReactNode }> = ({ children }) => {
+    const value = useSettingsStorage();
+    return (
+        <SettingsStorageContext.Provider 
+            value={value}
+        >
+            {children}
+        </SettingsStorageContext.Provider>
+    );
+};
+
+export function useSettingsStorageContext() {
+    const context = useContext(SettingsStorageContext);
+    if (!context) {
+        throw new Error('useSettingsStorageContext must be used within a SettingsStorageProvider');
+    }
+    return context;
+}
