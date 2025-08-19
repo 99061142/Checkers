22c539ea8654b836cdae45fc5463d0c9fd3954b8
99061142 <70692479+99061142@users.noreply.github.com>
2025-08-19 05:16:35 +0200

    Refactor EscapeMenu to functional component
    * Converted EscapeMenu from a class component to a functional component.

--- src/Checkers/EscapeMenu.tsx
+++ src/Checkers/EscapeMenu.tsx
@@ -1,105 +1,102 @@
-import { Component } from 'react';
+import { FC, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
+import { ComponentName } from './Window';
 
 /**
  * Props for the EscapeMenu component.
- * - toggleComponent: Function to toggle the current component.
+ * - `toggleComponent`: Function to toggle the current component.
  */
 interface EscapeMenuProps {
-    toggleComponent: (componentName: string) => void;
+    toggleComponent: (componentName: ComponentName) => void;
 }
 
-class EscapeMenu extends Component<EscapeMenuProps> {
-    constructor(props: EscapeMenuProps) {
-        super(props);
-        this.keyPressed = this.keyPressed.bind(this);
-    }
-
-    componentDidMount() {
-        window.addEventListener('keydown', this.keyPressed);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.keyPressed);
-    }
-
+const EscapeMenu: FC<EscapeMenuProps> = (props) => {
+    const { 
+        toggleComponent 
+    } = props;
 
     /**
-     * Handles key presses in the Escape Menu.
-     * @param {KeyboardEvent} ev - The keyboard event triggered by the user.
-     * @returns {Promise<void>} - A promise that resolves when the key press is handled.
+     * - Adds the keydown event listener and handler when the component mounts.
+     * - Removes the keydown event listener when the component unmounts.
      */
-    async keyPressed(ev: KeyboardEvent): Promise<void> {
-        // Yield the event loop to ensure the event is processed correctly
-        await Promise.resolve();
-
-        // If the user presses the Escape key, toggle the Game component to resume the game
-        if (ev.key === "Escape") {
-            this.props.toggleComponent("game");
-        }
-    }
-
-    render() {
-        const buttonStyling = {
-            background: "linear-gradient(45deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000), linear-gradient(135deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000)",
-            border: "3px #000 solid",
-            fontSize: "2vw",
-            color: "black"
+    useEffect(() => {
+        /**
+         * Handler for the keydown event.
+         * - If the Escape key is pressed, it toggles the component to 'game'.
+         * @param {KeyboardEvent} ev - The keyboard event.
+         * @returns {void}
+         */
+        const keydownHandler = (ev: KeyboardEvent): void => {
+            const pressedKey = ev.key;
+            if (pressedKey === 'Escape') {
+                toggleComponent('game');
+            }
         };
+        
+        window.addEventListener('keydown', keydownHandler);
+        return () => {
+            window.removeEventListener('keydown', keydownHandler);
+        };
+    }, [toggleComponent]);
 
-        return (
-            <div
-                data-testid="escapeMenu"
-                className="d-flex flex-column justify-content-center align-items-center"
+    const buttonStyling = {
+        background: 'linear-gradient(45deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000), linear-gradient(135deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000)',
+        border: '3px #000 solid',
+        fontSize: '2vw',
+        color: 'black'
+    };
+    return (
+        <div
+            data-testid='escapeMenu'
+            className='d-flex flex-column justify-content-center align-items-center'
+            style={{
+                height: '100vh',
+                width: '100vw',
+                background: 'linear-gradient(45deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000), linear-gradient(135deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000)'
+            }}
+        >
+            <h1
+                className='text-center'
                 style={{
-                    height: "100vh",
-                    width: "100vw",
-                    background: "linear-gradient(45deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000), linear-gradient(135deg, #000 25%, transparent 25%, transparent 75%, #000 75%, #000)"
+                    fontSize: '7.5vw',
+                    marginBottom: '10vh'
                 }}
             >
-                <h1
-                    className="text-center"
-                    style={{
-                        fontSize: "7.5vw",
-                        marginBottom: "10vh"
-                    }}
+                Escape Menu
+            </h1>
+            <div
+                className='d-flex gap-5 flex-column'
+                style={{
+                    width: '40vw'
+                }}
+            >
+                <Button
+                    className='rounded-5 py-4'
+                    data-testid='escapeMenuResumeButton'
+                    onClick={() => toggleComponent('game')}
+                    style={buttonStyling}
                 >
-                    Escape Menu
-                </h1>
-                <div
-                    className="d-flex gap-5 flex-column"
-                    style={{
-                        width: "40vw"
-                    }}
+                    Resume Game
+                </Button>
+                <Button
+                    className='rounded-5 py-4'
+                    data-testid='escapeMenuSettingsButton'
+                    onClick={() => toggleComponent('settings')}
+                    style={buttonStyling}
                 >
-                    <Button
-                        className="rounded-5 py-4"
-                        data-testid="escapeMenuResumeButton"
-                        onClick={() => this.props.toggleComponent("game")}
-                        style={buttonStyling}
-                    >
-                        Resume Game
-                    </Button>
-                    <Button
-                        className="rounded-5 py-4"
-                        data-testid="escapeMenuSettingsButton"
-                        onClick={() => this.props.toggleComponent("settings")}
-                        style={buttonStyling}
-                    >
-                        Settings
-                    </Button>
-                    <Button
-                        className="rounded-5 py-4"
-                        data-testid="escapeMenuQuitButton"
-                        onClick={() => this.props.toggleComponent("mainMenu")}
-                        style={buttonStyling}
-                    >
-                        Main Menu
-                    </Button>
-                </div>
+                    Settings
+                </Button>
+                <Button
+                    className='rounded-5 py-4'
+                    data-testid='escapeMenuQuitButton'
+                    onClick={() => toggleComponent('mainMenu')}
+                    style={buttonStyling}
+                >
+                    Main Menu
+                </Button>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default EscapeMenu;
