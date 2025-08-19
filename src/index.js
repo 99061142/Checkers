7762891d1d79842ac43e801eb33ae689fdcb1e1a
99061142 <70692479+99061142@users.noreply.github.com>
2025-08-19 04:36:39 +0200

    Refactor to use named StrictMode import
    Replaces the default React import with a named StrictMode import.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './Checkers/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <StrictMode>
         <App />
-    </React.StrictMode>
+    </StrictMode>
 );
