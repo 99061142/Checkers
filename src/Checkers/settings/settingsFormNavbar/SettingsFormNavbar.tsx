3545983edd6a635907e58819f21d33f6b97a1852
99061142 <70692479+99061142@users.noreply.github.com>
2026-01-22 23:39:29 +0100

    Renamed variables & made comment more clear

--- src/Checkers/settings/settingsFormNavbar/SettingsFormNavbar.tsx
+++ src/Checkers/settings/settingsFormNavbar/SettingsFormNavbar.tsx
@@ -7,7 +7,7 @@ import styles from './SettingsFormNavbar.module.scss';
 /**
  * Props for the Settings Form Nav Bar.
  * - `currentFormName`: The name of the currently displayed settings form.
- * - `setCurrentFormName`: Function to set the current settings form name. This will switch the displayed settings form.
+ * - `setCurrentFormName`: Function to set the current settings form name. This will update the displayed form.
  */
 interface SettingsFormNavbarProps {
     currentFormName: SettingName | null;
@@ -24,22 +24,22 @@ const SettingsFormNavbar: FC<SettingsFormNavbarProps> = ({
                 className={styles.formNav}
             >
                 {settingsFormNamesArray.map((settingsFormName) => {
-                    const settingNameCapitalized = settingsFormName.charAt(0).toUpperCase() + settingsFormName.slice(1);
-                    const isSettingShown = currentFormName === settingsFormName;
+                    const settingsFormNameCapitalized: string = settingsFormName.charAt(0).toUpperCase() + settingsFormName.slice(1);
+                    const isSettingsFormShown: boolean = currentFormName === settingsFormName;
                     return (
                         <NavLink
                             as={Button}
                             className='text-dark bg-transparent'
                             style={{
-                                fontWeight: isSettingShown ? 'bold' : 'normal',
+                                fontWeight: isSettingsFormShown ? 'bold' : 'normal',
                             }}
-                            data-testid={`settingsFormLink${settingNameCapitalized}`}
-                            disabled={isSettingShown}
+                            data-testid={`settingsFormLink${settingsFormNameCapitalized}`}
+                            disabled={isSettingsFormShown}
                             draggable={false}
                             onClick={() => setCurrentFormName(settingsFormName)}
                             key={settingsFormName}
                         >
-                            {settingNameCapitalized}
+                            {settingsFormNameCapitalized}
                         </NavLink>
                     )
                 })}
