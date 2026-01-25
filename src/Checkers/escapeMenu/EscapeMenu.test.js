fffb726f47bb07792b402f8af9b43638bc963913
99061142 <70692479+99061142@users.noreply.github.com>
2026-01-25 20:53:55 +0100

    Remove unecessary waitFor import

--- src/Checkers/escapeMenu/EscapeMenu.test.js
+++ src/Checkers/escapeMenu/EscapeMenu.test.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, act, waitFor } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import { renderWithProviders } from '../../utils/test/renderWithProviders.tsx';
 import EscapeMenu from './EscapeMenu.tsx';
 import UIRoot from '../ui/UIRoot.tsx';
