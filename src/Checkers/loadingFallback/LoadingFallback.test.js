3b9fe12455bd6291ef24d5475253d7cd88c8e69f
99061142 <70692479+99061142@users.noreply.github.com>
2026-01-31 17:54:01 +0100

    Removed non-needed act / try/expect

--- src/Checkers/loadingFallback/LoadingFallback.test.js
+++ src/Checkers/loadingFallback/LoadingFallback.test.js
@@ -1,21 +1,10 @@
 import '@testing-library/jest-dom';
-import { render, act } from '@testing-library/react';
 import { renderWithProviders } from '../utils';
 import LoadingFallback from './LoadingFallback.tsx';
 
-// Tests if the LoadingFallback component renders correctly on mount
-describe("LoadingFallback renders on mount", () => {
-    test("LoadingFallback renders without crashing when the application is initialized", async () => {
-        try {
-            await act(async () => {
-                render(
-                    renderWithProviders(
-                        <LoadingFallback />
-                    )
-                );
-            });
-        } catch (error) {
-            throw new Error(`LoadingFallback failed to render on mount: ${error}`);
-        }
-    });
+/* Test whether the loading fallback component renders without crashing */
+test("Renders without crashing", () => {
+    renderWithProviders(
+        <LoadingFallback />
+    );
 });
